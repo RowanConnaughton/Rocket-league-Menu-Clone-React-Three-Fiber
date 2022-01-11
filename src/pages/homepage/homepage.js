4c50c6ff8fc9beae33edcb938512c7741f0de97b
Rowan Connaughton <rowanconnaughton@gmail.com>
2022-01-11 16:06:09 +0100

    map added

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,102 +1,88 @@
-import { useState } from "react"
+import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Environment, PerspectiveCamera } from "@react-three/drei";
-// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-// import { useLoader } from "@react-three/fiber";
-import DatGui, { DatNumber } from 'react-dat-gui';
-
-
+import {
+  OrbitControls,
+  Environment,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import DatGui, { DatNumber } from "react-dat-gui";
 
 import Menu from "../../components/menu/menu";
 
 import FennecModel from "../../components/models/Fennec";
 import BallModel from "../../components/models/Ball";
+import StadiumModel from "../../components/models/Stadium";
 
 import "./homepage.styles.scss";
-import 'react-dat-gui/dist/index.css';
-
-
+import "react-dat-gui/dist/index.css";
 
 const HomePage = () => {
-  // const gltf = useLoader(GLTFLoader, "models/stadium/scene.gltf");
-
   const [Cposition, setCposition] = useState({
     positionx: -1.4,
     positiony: 0.2,
-    positionz: 1.9
+    positionz: 1.9,
   });
 
-
-  
- // camera={{ fov: 50, near: 0.1, far: 40, position: [-1.8, 0, 1.6] }}
+  // camera={{ fov: 50, near: 0.1, far: 40, position: [-1.8, 0, 1.6] }}
   return (
     <div className="container-home">
-        <Menu/>
-      <Canvas id="canvas"
-        flat
-        dpr={[1, 2]}
-        shadows
-      >
-
-        <PerspectiveCamera makeDefault position={[Cposition.positionx, Cposition.positiony, Cposition.positionz]} />
-
-        <Environment
-          background={true} // Whether to affect scene.background
-          files={[
-            "posx.jpg",
-            "negx.jpg",
-            "posy.jpg",
-            "negy.jpg",
-            "posz.jpg",
-            "negz.jpg",
-          ]} // Array of cubemap files OR single equirectangular file
-          path={"/resources/"} // Path to the above file(s)
+      <Menu />
+      <Canvas id="canvas" flat dpr={[1, 2]} shadows>
+        <PerspectiveCamera
+          makeDefault
+          position={[
+            Cposition.positionx,
+            Cposition.positiony,
+            Cposition.positionz,
+          ]}
         />
 
-      <ambientLight intensity={0.5} />
-      <spotLight
-        intensity={0.8}
-        position={[0, 10, 15]}
-        penumbra={1}
-        shadow-mapSize-Width={2048}
-        shadow-mapSize-Height={2048}
-        castShadow
-      />
+        <Environment background={true} files="/resources/flower_road_1k.hdr" />
 
-
+        <ambientLight intensity={0.5} />
+        <spotLight
+          intensity={0.8}
+          position={[0, 10, 15]}
+          penumbra={1}
+          shadow-mapSize-Width={2048}
+          shadow-mapSize-Height={2048}
+          castShadow
+        />
 
         <OrbitControls
           enablePan={true}
-          enableZoom={false}
+          enableZoom={true}
           enableRotate={true}
           minPolarAngle={0}
           maxPolarAngle={Math.PI / 2}
         />
 
-        {/* <primitive
-          object={gltf.scene}
+        <StadiumModel
+        receiveShadow
           position={[0, -0.5, 0]}
           rotation={[0, 400, 0]}
-          scale={[20, 20, 20]}
-          receiveShadow
-        /> */}
+          scale={[0.02, 0.02, 0.02]}
+          
+        />
 
         <FennecModel
-          
+          castShadow
           position={[0, -0.5, 0]}
           rotation={[0, 500, 0]}
           scale={[0.01, 0.01, 0.01]}
         />
-       
-       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -.5, 0]} receiveShadow>
+
+        {/* <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -.5, 0]} receiveShadow>
         <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
         <meshPhongMaterial attach="material" color="#d1cebd" />
-      </mesh>
+      </mesh> */}
 
-     
-
-        <BallModel  position={[5, .5, -2]} rotation={[0, 0, 0]} scale={[0.01, 0.01, 0.01]} />
-        
+        <BallModel
+          castShadow
+          position={[5, 0.5, -2]}
+          rotation={[0, 0, 0]}
+          scale={[0.01, 0.01, 0.01]}
+        />
       </Canvas>
       <DatGui data={Cposition} onUpdate={setCposition}>
         <DatNumber
@@ -120,8 +106,6 @@ const HomePage = () => {
           max={10}
           step={0.1}
         />
-        
-        
       </DatGui>
     </div>
   );
