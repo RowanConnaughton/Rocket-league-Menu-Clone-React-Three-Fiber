bb933acdef2836f7a96c122187d822fb261ad495
Rowan Connaughton <rowanconnaughton@gmail.com>
2022-01-28 14:22:11 +0100

    added replay search page

---
+++ src/pages/replay/replay.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+//import components
+import Card from "../../components/card/card";
+import Logo from "../../components/logo/logo";
+
+//import stlyes
+import "./replay.styles.scss";
+
+const replay = () => {
+  return (
+    <div className="container">
+    <Logo/>
+
+    <Card title={'Search Replays'} >
+    search
+    </Card>
+        
+    </div>);
+};
+
+export default replay;
