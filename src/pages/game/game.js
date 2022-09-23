5acf74b25b297d82cb123d5d3da516f24179c8a6
Rowan Connaughton <rowanconnaughton@gmail.com>
2022-09-23 12:52:04 +0200

    fix dependancy conflict

---
+++ src/pages/game/game.js
@@ -0,0 +1,9 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Physics, useCylinder, usePlane } from "@react-three/cannon";
+
+const game = () => {
+  return <div>game</div>;
+};
+
+export default game;
